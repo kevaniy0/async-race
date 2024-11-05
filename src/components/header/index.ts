df55ea732ed169b0b4c8261730406c902064e8dd
kevaniy0 <sevak_08@mail.ru>
2024-11-05 12:43:08 +0300

    feat: implement menu for switch view

--- src/components/header/index.ts
+++ src/components/header/index.ts
@@ -1,12 +1,14 @@
 import './style.scss';
 import { createElemet } from '../../utils/dom';
 import { headerParams, titleParams } from './header-data';
+import Menu from '../menu';
 
 const Header = (): HTMLElement => {
     const header = createElemet(headerParams);
     const title = createElemet(titleParams);
+    const menu = Menu();
 
-    header.append(title);
+    header.append(title, menu);
 
     return header;
 };
