65ab0ceaa0cad0bf7ba0c9c1dba3254af790c5e5
Vsevolod Goriachev <sevak_08@mail.ru>
2024-11-26 10:52:03 +0300

    feat: add functionality for reset button

--- src/components/engineButtons/blockEngineButtons.ts
+++ src/components/engineButtons/blockEngineButtons.ts
@@ -1,3 +1,5 @@
+import { getElement } from '../../utils/dom';
+
 const blockEngineButtons = () => {
     const startButtons = Array.from(
         document.body.querySelectorAll('.engine-button__start')
@@ -10,6 +12,25 @@ const blockEngineButtons = () => {
     ) as HTMLButtonElement[];
     stopButtons.forEach((item) => {
         item.removeAttribute('disabled');
+    });
+};
+
+export const unBlockEngineButtons = () => {
+    const raceButton = getElement('.button__race') as HTMLButtonElement;
+    if (raceButton.disabled) {
+        raceButton.disabled = false;
+    }
+    const startButtons = Array.from(
+        document.body.querySelectorAll('.engine-button__start')
+    ) as HTMLButtonElement[];
+    startButtons.forEach((item) => {
+        item.removeAttribute('disabled');
+    });
+    const stopButtons = Array.from(
+        document.body.querySelectorAll('.engine-button__stop')
+    ) as HTMLButtonElement[];
+    stopButtons.forEach((item) => {
+        item.setAttribute('disabled', 'true');
     });
 };
 
